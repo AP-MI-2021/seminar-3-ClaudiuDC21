52e5a1ea85f22573ca29132af4cd07688f591ba2
Claudiu <claudiu.dascalu@stud.ubbcluj.ro>
2021-10-19 13:40:42 +0300

    da

--- repetare.py
+++ repetare.py
@@ -1,3 +1,5 @@
+from typing import List
+
 def show_menu():
     print('1. Citirea unei liste cu n siruri de caracetere. ')
     print('2. Determinarea daca un anumit sir se gaseste in lista. ')
@@ -14,6 +16,23 @@ def read_list():
         lista_str.append(str(i))
     return lista_str
 
+def get_str_in_list(lst: List[str], n: str):
+    '''
+    Determina daca unsir de caractere se afla in lista
+    :param lst: O lista data
+    :param n: Sirul cautat
+    :return: True daca sirul cautat este in lista, Fals in caz contrar.
+    '''
+    for i in lst:
+        if n == i:
+            return True
+    return False
+
+def test_get_str_in_list():
+    assert get_str_in_list(['aaa', 'bbb', 'cmtc', 'drd', 'aaa'], 'drd') == True
+    assert get_str_in_list([], 'asd') == False
+    assert get_str_in_list(['aaa', 'bbb', 'cmtc', 'drd', 'aaa'], 'abc') == False
+
 def main():
     lista = []
     while True:
@@ -22,7 +41,11 @@ def main():
         if optiunea == '1':
             lista = read_list()
         elif optiunea == '2':
-            pass
+            sir = str(input('Scrieti sirul de caractere comparabil: '))
+            if get_str_in_list(lista, sir):
+                print('DA')
+            else:
+                print('NU')
         elif optiunea == '3':
             pass
         elif optiunea == '4':
@@ -36,4 +59,5 @@ def main():
 
 
 if __name__ == '__main__':
+    test_get_str_in_list()
     main()
